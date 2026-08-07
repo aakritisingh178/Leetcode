3368ac58f0d16d204cd48725fc002d307a0fdd71
aakritisingh178 <aakritisingh3598@gmail.com>
2026-08-07 19:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (50.76%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -7,11 +7,9 @@ public:
         vector<int>ans;
         for(int i=0;i<n;i++){
            x = nums[i];
-           while( i<n && x == nums[i] ){
-            i++;
-            count++;
-           }
-           i--;
+           while( i<n && x == nums[i] )
+            i++,count++;    
+            i--;
            if(count>time) ans.push_back(nums[i]);
            count =0;
         }
